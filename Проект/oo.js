39ed3244271aa59ff9712c81a9e3d8881f9911d8
Alexander <164621370+Sanya8821@users.noreply.github.com>
2024-06-14 11:09:27 +0300

    My school project for 9 class

---
+++ Проект/oo.js
@@ -0,0 +1,36 @@
+function btn_p1(){
+    document.location="proekt.html"
+}
+function btn_p2(){
+    document.location="proekt2.html"
+}
+function btn_Title(){
+    document.location="Title.html"
+}
+function btn_ogl(){
+    document.location="oglav.html"
+}
+function btn_drug(){
+    document.location="dryg.html"
+}
+function btn_Blag(){
+    document.location="Blago.html"
+}
+function btn_Fish(){
+    document.location="Fish.html"
+}
+function btn_pobeda(){
+    document.location="pobeda.html"
+}
+function btn_pira(){
+    document.location="piramida.html"
+}
+function i(){
+    document.location="Данные опроса.html"
+}
+function j(){
+    document.location="Vivod.html"
+}
+function u(){
+    document.location="info.html"
+}
